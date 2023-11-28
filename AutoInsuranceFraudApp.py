e138476827dc4f4755042d2d32fe61e174f60521
nilpiyu <priyaswt@gmail.com>
2023-11-28 14:37:02 +0530

    Update AutoInsuranceFraudApp.py

--- AutoInsuranceFraudApp.py
+++ AutoInsuranceFraudApp.py
@@ -1,5 +1,5 @@
-⁸import streamlit as st
-import numpy as np # linear algebra
+import streamlit as st
+ipmport numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import pickle
 import sklearn
