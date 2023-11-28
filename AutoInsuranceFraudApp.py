4ea75c5b41507a180aae841ec70f010eac0f2fcf
nilpiyu <priyaswt@gmail.com>
2023-11-28 14:00:19 +0530

    Update AutoInsuranceFraudApp.py

--- AutoInsuranceFraudApp.py
+++ AutoInsuranceFraudApp.py
@@ -107,7 +107,7 @@ def predict(data):
     return clf.predict(data)
 
 # df_new_data =pd.read_csv('Automobile_insurance_fraud_v1.csv')
-df_new_data = data
+df_new_data = input_df
 # Apply model to make predictions
 st.write(df_new_data)
 # preprocessing steps
